a0174b6c9048fb4a44c4279e60cecf0be286145e
NinyoRaleigh <ninoraleighparagas.com>
2025-09-09 21:15:22 +0800

    Added v-if and v-else in Add New Task

--- src/components/NewAddedTask.vue
+++ src/components/NewAddedTask.vue
@@ -2,7 +2,37 @@
 import NavBar from '../components/NavBar.vue';
 import SideBar from '../components/SideBar.vue';
 import TaskCard from '../components/TaskCard.vue';
+import { ref } from 'vue';
 
+const todos = ref([
+    
+]);
+
+const newTodo = ref("");
+
+function addTodo() {
+    todos.value.push({
+        id: Math.random(),
+        name: newTodo.value,
+        isComplete : false,
+    });
+    newTodo.value = "";
+}
+
+function markAsComplete(id) {
+    const todo = todos.value.find((todo) => todo.id === id);
+
+    if (todo) {
+        todo.isComplete = true;
+    }
+}
+
+function undoTodo(id) {
+    const todo = todos.value.find((todo) => todo.id === id)
+    if (todo) {
+        todo.isComplete = false;
+    }
+}
 
 </script>
 
@@ -18,11 +48,31 @@ import TaskCard from '../components/TaskCard.vue';
             <SideBar></SideBar>
         </aside>
 
-        <div class="absolute left-1/3 top-1/5 bottom-1/4 right-1/3 bg-white rounded-2xl ">
+        <div class="absolute left-1/3 top-1/5 bottom-1/4 right-1/3 bg-white rounded-2xl space-y-4">
             <div class="text-center ">
-                <h1>Added Tasks</h1>
+                <h1 class="p-2 text-lg font-semibold">Added Tasks</h1>
             </div>
+            <div>
+                <div class="m-5">
+            <ul>
+                <li v-for="(todo, index) in todos" class="flex space-x-3 items-center">
+                    <h1 :class="todo.isComplete ? 'line-through' : ''">
+                        {{ todo.name }}
+                    </h1>
+                    <button 
+                    v-if="todo.isComplete === false"
+                    @click="markAsComplete(todo.id)" class="text-green-500">Complete</button>
+                    <button 
+                    v-else @click="undoTodo(todo.id)" class="text-green-500">Undo</button>
+                </li>
+            </ul>
+        </div>
+        <div class="ml-5">
+            <input v-model="newTodo" type="text" class="border" />
+            <button @click="addTodo">Add</button>
+        </div>
 
+            </div>
         </div>
     </div>
 
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,17 +1,41 @@
 <script setup>
 import SideButton from './SideButton.vue';
+import { ref } from 'vue';
+
+const todos = ref([
+
+]);
+
+const newTodo = ref("");
+
+function addTodo () {
+    todos.value.push({
+        id: Math.random(),
+        name: newTodo.value,
+        isComplete : false,
+    });
+    newTodo.value = "";
+}
+
 
 </script>
 
 <template>
     <div class="justify-between flex p-10 border-b">
-        <input type="text" placeholder="Add a new task..."
+        <input v-model="newTodo" type="text" placeholder="Add a new task..."
             class=" rounded-lg border-2 border-gray-300 bg-white p-3 w-2/3 focus:outline-none focus:border-indigo-600 focus:border-3 overflow-ellipsis">
-        <SideButton class="px-8 font-semibold text-lg">Add Task</SideButton>
+        <button @click="addTodo"><SideButton class="px-8 font-semibold text-lg">Add Task</SideButton></button>
     </div>
 
-    <div class="m-5 p-2 bg-gray-100 border-gray-400 border rounded-lg italic text-gray-500">
-        <p class="">No tasks yet. Add one above!</p>
+    <div v-if="todos.length === 0" 
+    class="m-8 p-2 bg-gray-100 border-gray-400 border rounded-lg italic text-gray-500">
+        <p class="">No tasks yet. Add one above!</p> remove this if new task are added 
+    </div>
+    <div v-else class="space-y-2 m-8 p-2 bg-gray-100 border-gray-400 border rounded-lg italic text-black   h-[60%] overflow-y-auto">
+        <div v-for="todo in todos" :key="todo.id"  class="p-2 bg-white border rounded-lg flex items-center justify-between">
+            <span>{{ todo.name }}</span>
+        </div>
+
     </div>
 
 </template>
